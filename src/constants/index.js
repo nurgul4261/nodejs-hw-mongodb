ea35c38f9a43d239c24dc364b446ebb3d005d395
nurgul4261 <nurguloksuzoglu@gmail.com>
2026-03-10 18:16:31 +0300

    Replace ONE_DAY_IN_MS with THIRTY_DAYS_IN_MS constant

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,5 +1,5 @@
 export const FIFTEEN_MINUTES_IN_MS = 15 * 60 * 1000;
-export const ONE_DAY_IN_MS = 24 * 60 * 60 * 1000;
+export const THIRTY_DAYS_IN_MS = 30 * 24 * 60 * 60 * 1000;
 
 export const USER_ROLES = {
     USER: "user",
